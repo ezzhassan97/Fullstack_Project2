c69c6606f3fb95ec49a738ddc9308b8c5b0636a2
Ezz Hassan <89714476+ezzhassan97@users.noreply.github.com>
2022-03-11 00:59:06 +0200

    Adding Unit test for Orders endpoints

--- src/tests/endpoints/orders.endpointSpec.ts
+++ src/tests/endpoints/orders.endpointSpec.ts
@@ -1,94 +1,77 @@
-// import supertest from "supertest";
-// import app from "../../server";
-// import userModel from "../../models/user.model";
+import supertest from "supertest";
+import app from "../../server";
+import orderModel from "../../models/order.model";
+import userModel from "../../models/user.model";
+import Order from "../../types/order.type";
+import User from "../../types/user.type";
+import db from "../../database/database";
 
-// import User from "../../types/user.type";
-// import db from "../../database/database";
-// import Order from "../../types/order.type";
+const userM = new userModel();
+const orderM = new orderModel();
 
-// const request = supertest(app);
-// let token = "";
+const request = supertest(app);
+let token = "";
 
-// const userM = new userModel();
+describe("Testing Logic", () => {
+	const user = {
+		username: "testname",
+		firstname: "firsttest",
+		lastname: "lasttest",
+		password: "testpassword",
+	} as User;
 
-// describe(" Users API Endpoint ", () => {
-// 	const user = {
-// 		username: "testname",
-// 		firstname: "firsttest",
-// 		lastname: "lasttest",
-// 		password: "testpassword",
-// 	} as User;
+	const order = {} as Order;
 
-// 	beforeAll(async () => {
-// 		const createdUser = await userM.create(user);
-// 		user.id = createdUser.id;
-// 		console.log(user.id);
-// 	});
+	beforeAll(async () => {
+		const createdUser = await userM.create(user);
 
-// 	afterAll(async () => {
-// 		const conn = await db.connect();
-// 		const sql = "DELETE FROM users;";
-// 		await conn.query(sql);
-// 		conn.release();
-// 	});
-// 	it("User Authenticate Endpoint", async () => {
-// 		const res = await request
-// 			.post("/api/users/authenticate")
-// 			.set("content-type", "application/json")
-// 			.send({ username: "testname", password: "testpassword" });
-// 		expect(res.status).toBe(200);
+		user.id = createdUser.id;
+		order.user_id = createdUser.id as unknown as number;
+		const createdOrder = await orderM.create(order.user_id);
+	});
 
-// 		const username = res.body.username;
-// 		const firstname = res.body.firstname;
-// 		const lastname = res.body.lastname;
-// 		token = res.body.token;
+	afterAll(async () => {
+		const conn = await db.connect();
+		const sql2 = "DELETE FROM orders;";
+		await conn.query(sql2);
+		const sql1 = "DELETE FROM users;";
+		await conn.query(sql1);
 
-// 		expect(username).toEqual(user.username);
-// 		expect(firstname).toEqual(user.firstname);
-// 		expect(lastname).toEqual(user.lastname);
-// 	});
+		conn.release();
+	});
+	it("Order Create Endpoint", async () => {
+		const res = await request
+			.post("/api/orders")
+			.set("content-type", "application/json")
+			.send({ user_id: order.user_id });
+		expect(res.status).toBe(200);
 
-// 	it("User Create Endpoint", async () => {
-// 		const res = await request
-// 			.post("/api/orders")
-// 			.set("content-type", "application/json")
+		const id = res.body.id;
+		const user_id = res.body.user_id;
 
-// 			.send({
-// 				user_id: user.id,
-// 			} as Order);
-// 		expect(res.status).toBe(200);
+		expect(id).toEqual(order.id);
+		expect(user_id).toEqual(order.user_id);
+	});
 
-// 		const id = res.body.id;
-// 		const user_id = res.body.user_id;
+	it("Order Show Endpoint", async () => {
+		const res = await request
+			.get(`/api/orders/${order.id}`)
+			.set("content-type", "application/json");
+		expect(res.status).toBe(200);
 
-// 		expect(username).toEqual("testname2");
-// 		expect(firstname).toEqual("firsttest");
-// 	});
+		const id = res.body.id;
+		const user_id = res.body.user_id;
 
-// 	it("User Show Endpoint", async () => {
-// 		const res = await request
-// 			.get(`/api/users/${user.id}`)
-// 			.set("content-type", "application/json")
-// 			.set("Authorization", `Bearer ${token}`);
-// 		expect(res.status).toBe(200);
+		expect(id).toBe(order.id);
+		expect(user_id).toBe(order.user_id);
+	});
 
-// 		const id = res.body.id;
-// 		const username = res.body.username;
-// 		const firstname = res.body.firstname;
-// 		const lastname = res.body.lastname;
-// 		expect(id).toBe(user.id);
-// 		expect(username).toBe("testname");
-// 		expect(firstname).toBe("firsttest");
-// 		expect(lastname).toBe("lasttest");
-// 	});
-
-// 	it("User Index Endpoint", async () => {
-// 		const res = await request
-// 			.get("/api/users")
-// 			.set("content-type", "application/json")
-// 			.set("Authorization", `Bearer ${token}`);
-// 		expect(res.status).toBe(200);
-// 		const allUsers = res.body;
-// 		expect(allUsers.length).toEqual(2);
-// 	});
-// });
+	it("order Index Endpoint", async () => {
+		const res = await request
+			.get("/api/orders")
+			.set("content-type", "application/json");
+		expect(res.status).toBe(200);
+		const allOrders = res.body;
+		expect(allOrders.length).toEqual(2);
+	});
+});
